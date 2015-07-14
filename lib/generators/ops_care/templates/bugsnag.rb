198f3f9ab3da36f869719be8ec403e2cd55615a1
Raphael Campardou <ralovely@gmail.com>
2015-07-14 10:50:43 +1000

    Initialize Bugsnag in staging or production only

--- lib/generators/ops_care/templates/bugsnag.rb
+++ lib/generators/ops_care/templates/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = ENV['BUGSNAG_KEY']
+if Rails.env.production? || Rails.env.staging?
+  Bugsnag.configure do |config|
+    config.api_key = ENV['BUGSNAG_KEY']
+  end
 end
