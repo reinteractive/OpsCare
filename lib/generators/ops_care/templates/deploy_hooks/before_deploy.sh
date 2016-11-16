622cbd3b8ac274a65f973934a9ec3e257b60006a
Raphael Campardou <ralovely@gmail.com>
2016-11-16 13:21:28 +1100

    Update whenever hook service

--- lib/generators/ops_care/templates/deploy_hooks/before_deploy.sh
+++ lib/generators/ops_care/templates/deploy_hooks/before_deploy.sh
@@ -9,3 +9,8 @@
 # stop_delayed_job
 # # or
 # stop_resque
+
+# # For Whenever-based crons
+# # schedule.rb file should have this line at the top:
+# # set :job_template, "bash -l -c '[[ ! -f /tmp/STOP_CRONS ]] && . /etc/app_description && . $APP_LOCATION/shared/envvars && :job'"
+# wheneverize_worker
